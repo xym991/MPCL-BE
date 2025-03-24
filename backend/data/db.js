4e7dadebb6675b2255f4462472ed8a8afbc11d97
Muhammad Shahzer <muhammadshahzer991@gmail.com>
2025-03-25 05:29:07 +0530

    --

--- backend/data/db.js
+++ backend/data/db.js
@@ -1,9 +1,15 @@
 const mysql = require("mysql2");
+const fs = require("fs");
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "password123",
-  database: "mpcl",
+  database: process.env.DB_NAME || "mpcl",
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    cert: fs.readFileSync(__dirname + "/ca.pem"),
+    rejectUnauthorized: false, // Enf
+  },
 });
 
 db.connect((err) => {
